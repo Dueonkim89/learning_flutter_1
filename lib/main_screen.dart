61c61b8b8f37e9621d88229a8b370f2f0730be05
unknown <dueonkim89@Yahoo.com>
2022-02-01 18:32:14 -0800

    asset usage for flutter

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -11,14 +11,20 @@ class MainScreen extends StatelessWidget {
         appBar: AppBar(title: Text(title)),
         body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [placeHolderRow(), placeHolderRow()]));
+            children: [banner(context), placeHolderRow(), placeHolderRow()]));
   }
+}
+
+Widget banner(BuildContext context) {
+  return Text("Aliens!",
+      style: TextStyle(fontFamily: 'Syne Tactile', fontSize: 68));
 }
 
 Widget paddedPlaceholder() {
   return Padding(
       padding: EdgeInsets.all(20),
-      child: SizedBox(child: Placeholder(), width: 100, height: 100));
+      child: SizedBox(
+          child: Image.asset('assets/alien_guy.jpg'), width: 100, height: 100));
 }
 
 Widget placeHolderRow() {
